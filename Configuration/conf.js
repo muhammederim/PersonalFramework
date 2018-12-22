18c64e002d9c5dc6725889a389e0a5fe5f868ec7
muhammederim <muhammederim.a@gmail.com>
2018-12-21 19:41:09 -0500

    My first commit

---
+++ Configuration/conf.js
@@ -0,0 +1,18 @@
+exports.config = {
+
+    framework:'jasmine2',
+
+    directConnect : true,
+
+    capabilities: {
+
+        browserName: 'chrome'
+    },
+
+
+    specs: ['../Tests/Spec.spec1.js']
+
+
+
+
+}
---
+++ Tests/Spec.spec1.js
@@ -0,0 +1,20 @@
+
+
+
+describe('Testing login page',function(){
+
+    it('shoul navigate the page',function(){
+        browser.waitForAngularEnabled(false);
+        
+        browser.get('https://4stay.com/');
+        browser.sleep(4000);
+
+
+    })
+
+
+
+
+
+
+})
